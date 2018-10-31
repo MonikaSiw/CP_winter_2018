03632b9df4bac8ad13867fb1f3c75fc92248a400
MonikaSiw <monika.siw@gmail.com>
2018-10-31 13:13:01 +0100

    Adding argument to Main

--- src/pl/waw/sgh/BowlsRecursion.java
+++ src/pl/waw/sgh/BowlsRecursion.java
@@ -3,13 +3,21 @@ package pl.waw.sgh;
 import java.util.Scanner;
 
 public class BowlsRecursion {
+
     public static int sum(int parA, int parB) {
         int sum = parA + parB;
         return sum;
     }
 
-    public static void main(String[] args) {
 
+    public static int sumBowls(int row) {
+        if (row == 1) return 1;
+        else return sumBowls(row - 1) + row;
+    }
+
+
+
+    public static void main(String[] args) {
         /*
         int a ,b;
         System.out.print("Podaj ile wynosi a: ");
@@ -18,13 +26,12 @@ public class BowlsRecursion {
         System.out.print("Podaj ile wynosi b: ");
         Scanner Z=new Scanner(System.in);
         b=Z.nextInt();
-
         */
-
         int a = 3;
         int b = 5;
 
 
         System.out.println("sum is: " + sum(a, b));
+        System.out.println("Bowls in row 5: " + sumBowls(5));
     }
 }
